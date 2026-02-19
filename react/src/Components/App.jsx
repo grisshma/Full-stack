502b97de6e6e50c035e70bcd4ffa9da29b21bbb0
Grishma Sitaula <grishmasitaula07@gmial.com>
2026-02-19 13:13:04 +0545

    tailwind

---
+++ react/src/Components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Button from '../Button'
+
+const App = () => {
+  return (
+    <>
+    
+  <Button text={"Click"} color={red} />
+  <Button text={"More"} color={yello} />
+  <Button text={"Login"} color={blue} />
+  <Button text={"Register"} color={pink} />
+      </>
+  )
+}
+
+export default App
---
+++ react/src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      ndjcnj
+    </div>
+  )
+}
+
+export default Navbar
